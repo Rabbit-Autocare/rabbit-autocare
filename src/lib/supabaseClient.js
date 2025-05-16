f8206bed49645bf52af5a49d371bb08aa38eb8dd
YashInTech <YashInTech@users.noreply.github.com>
2025-05-16 15:05:44 +0530

    supabaseClient update

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bisrgvfnsoqrbrrilcnu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpc3JndmZuc29xcmJycmlsY251Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMDI0NzgsImV4cCI6MjA2Mjc3ODQ3OH0.9gZ5wfO3_McxvRSguwq40vFxiq0qlH6MS_ikSe3culU';
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

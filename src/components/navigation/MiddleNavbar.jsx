aae4227c52700e0d5ca82460a4584f290832639d
YashInTech <YashInTech@users.noreply.github.com>
2025-05-28 16:07:37 +0530

    Merged navbar components

--- src/components/navigation/MiddleNavbar.jsx
+++ src/components/navigation/MiddleNavbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 
 import Link from 'next/link';
@@ -19,26 +18,4 @@ export default function MiddleNavbar() {
       </div>
     </div>
   );
-=======
-"use client";
-
-import Link from "next/link";
-import Image from "next/image";
-
-export default function MiddleNavbar() {
-	return (
-		<div className="border-b py-4">
-			<div className="container mx-auto flex justify-center">
-				<Link href="/" className="text-4xl font-bold">
-					<Image
-						src="/assets/RabbitLogo.png"
-						alt="logo"
-						width={200}
-						height={200}
-					/>
-				</Link>
-			</div>
-		</div>
-	);
->>>>>>> 40c4643f54a9163bf182ae9d04607553a7807feb
 }

abe4f2e79cd86d485c30cbf11bd00e3460842414
PriyaGupta30302 <pria30march@gmail.com>
2025-06-03 17:24:08 +0530

    create featured products card design for desktop and mobile but gsap animtion bug is incomplete

---
+++ src/Data/featuredProductsData.js
@@ -0,0 +1,74 @@
+
+export const featuredProducts = [
+  {
+    id: 1,
+    image: '/assets/featured/mockup-spray-bottle.png',
+    thumbnails: [
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+    ],
+    heading: 'Windshield Cleaner',
+    ratings: 4,
+    mrp: 500,
+    description: `Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis..
+
+Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis.`,
+  },
+  
+  {
+    id: 2,
+    image: '/assets/featured/mockup-spray-bottle.png',
+    thumbnails: [
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+            '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+    ],
+    heading: 'Windshield Cleaner',
+    ratings: 4,
+    mrp: 500,
+    description: `Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis.
+
+Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis.`,
+  },
+
+  {
+    id: 3,
+    image: '/assets/featured/mockup-spray-bottle.png',
+    thumbnails: [
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+            '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+    ],
+    heading: 'Windshield Cleaner',
+    ratings: 4,
+    mrp: 500,
+    description: `Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis.
+
+Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis.`,
+  },
+
+  {
+    id: 4,
+    image: '/assets/featured/mockup-spray-bottle.png',
+    thumbnails: [
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+            '/assets/featured/mockup-spray-bottle.png',
+      '/assets/featured/mockup-spray-bottle.png',
+    ],
+    heading: 'Windshield Cleaner',
+    ratings: 4,
+    mrp: 500,
+    description: `Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis.
+
+Lorem ipsum dolor sit amet consectetur. Ut magna at fames volutpat etiam. Porttitor viverra integer pellentesque suspendisse quisque viverra etiam ridiculus in. Id id lobortis bibendum erat quam. Velit aliquet sit tempor vitae aliquam. Nisl in amet hendrerit duis imperdiet volutpat diam dictum. Imperdiet pellentesque viverra viverra purus. Ultrices tincidunt sagittis.`,
+  },
+];

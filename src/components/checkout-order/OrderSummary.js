d6054619dd5118c302615b713677a88d5df1154f
Yogesh55S <Yogesharma914@gmail.com>
2025-07-08 11:35:17 +0530

    checkout

--- src/components/checkout-order/OrderSummary.js
+++ src/components/checkout-order/OrderSummary.js
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image"
-import PriceSummary from "./PriceSummary";
+
 
 // Helper function to display variant info
 function getVariantDisplayText(variant, isMicrofiber) {

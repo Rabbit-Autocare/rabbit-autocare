a55c7f4abe3d9e06f65019f2b0310c0a223b119e
anurag262000 <anuragmishra262000@gmail.com>
2025-07-17 14:57:04 +0530

    layout fix

--- src/app/shop/[category]/page.jsx
+++ src/app/shop/[category]/page.jsx
@@ -582,7 +582,7 @@ export default function ShopPage({ initialCategories, initialError }) {
   }
 
   return (
-    <div className="bg-white pt-[100px] min-h-screen">
+    <div className="bg-white  min-h-screen">
       <BannerCarousel/>
       <div className="container mx-auto px-4 py-6 max-w-[1300px]">
         {/* Page Header */}
